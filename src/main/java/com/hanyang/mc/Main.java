f82e9be4c3df384c2c5e41bce590d8f1b9afb7b0
ydLee <game9777@naver.com>
2018-09-03 23:11:48 +0900

    Use java version 1.8 & Spring 1.5.9

--- src/main/java/com/hanyang/mc/Main.java
+++ src/main/java/com/hanyang/mc/Main.java
@@ -1,8 +1,22 @@
 package com.hanyang.mc;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello MACCLoud");
-    }
-}
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@Override
+	public void run(String...args) {
+			logger.info("Hello spring");
+			System.exit(0);
+	}
+}
